e20fe35e5e9317471a778737022583348416a8e4
tomot7 <tomotarou2020@outlook.jp>
2025-12-05 23:48:29 +0900

    init

---
+++ pyscript_libs/deprecations-manager-CWR-4gkk.js
@@ -0,0 +1,2 @@
+import{notify as e}from"./error-C4rw5PRJ.js";import{e as o}from"./core-DSF9czGY.js";function r(){const e=document.querySelectorAll("script");for(const o of e)s(o.src)}function s(o){/\/pyscript\.net\/latest/.test(o)&&e("Loading scripts from latest is deprecated and will be removed soon. Please use a specific version instead.")}o.main.onReady.add(r),o.main.onWorker.add(r);
+//# sourceMappingURL=deprecations-manager-CWR-4gkk.js.map
---
+++ start-server.bat
@@ -0,0 +1,3 @@
+@echo off
+npx http-server -p 8080
+pause
